de25b5074363d35b3bc154add17e8b7940b8c52d
sunyuluo <sun.yuluo@coscoshipping.com>
2026-08-07 17:24:19 +0800

    Update version.go

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Version is set by release builds and defaults to a development build marker.
-var Version = "dev"
+var Version = "0.30.0"
 
 // Commit is set by CI builds and defaults to an unknown revision marker.
 var Commit = "unknown"
